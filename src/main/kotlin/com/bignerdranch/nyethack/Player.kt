a23796f4885678983040fbd3cb1d30a79536cb16
Miko Belardo <embelardo@gmail.com>
2022-04-16 20:50:08 -0400

    Add ch14: Initialization

--- src/main/kotlin/com/bignerdranch/nyethack/Player.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Player.kt
@@ -1,8 +1,13 @@
 package com.bignerdranch.nyethack
 
-class Player {
+class Player(
+    initialName: String,
+    val hometown: String = "Neversummer",
+    var healthPoints: Int,
+    val isImmortal: Boolean
+) {
 
-    var name = "madrigal"
+    var name = initialName
         get() = field.replaceFirstChar { it.uppercase() }
         private set(value) {
             field = value.trim()
@@ -16,6 +21,33 @@ class Player {
             else -> "The Renowned Hero"
         }
 
+    val prophecy by lazy {
+        narrate("  $name embarks on an arduous quest to locate a fortune teller")
+        Thread.sleep(3000)
+        narrate("  The fortune teller bestows a prophecy upon $name")
+        "An intrepid hero from $hometown shall some day " + listOf(
+            "form an unlikely bond between two warring factions",
+            "take possession of an otherworldly blade",
+            "bring the gift of creation back to the world",
+            "best the world-eater"
+        ).random()
+    }
+
+    init {
+        require(healthPoints > 0) { "healthPoints must be greater than zero" }
+        require(name.isNotBlank()) { "Player must have a name" }
+    }
+
+    constructor(name: String) : this(
+        initialName = name,
+        healthPoints = 100,
+        isImmortal = false
+    ) {
+        if (name.equals("Jason", ignoreCase = true)) {
+            healthPoints = 500
+        }
+    }
+
     fun castFireball(numFireballs: Int = 2) {
         narrate("A glass of Fireball springs into existence (x$numFireballs)")
     }
@@ -23,6 +55,11 @@ class Player {
     fun changeName(newName: String) {
         narrate("$name legally changes their name to $newName")
         name = newName
+    }
+
+    fun prophesize() {
+        narrate("$name thinks about their future")
+        narrate("A fortune teller told Madrigal, \"$prophecy\"")
     }
 
 }
